12acc2afbb6486e9d72369035ec3a6bd1cd56111
chica25 <patricia@patriciaco.com>
2021-03-17 04:57:37 -0400

    Alter functions and html typos

--- form.js
+++ form.js
@@ -1,13 +1,16 @@
 function animatedForm() {
-    const arrows = document.querySelectorAll('.fas fa-arrow-down');
+    const arrows = document.querySelectorAll('.fa-arrow-down');
    
     arrows.forEach(arrow => {
         arrow.addEventListener('click', () => {
             const input = arrow.previousElementSibling;
+            console.log(input);
             const parent = arrow.parentElement;
             const nextForm = parent.nextElementSibling;
 
-            if(input.type === 'text' && validateUser(input))
+            if(input.type === 'text' && validateUser(input)) {
+                nextForm(parent, nextForm)
+            }
        
         })
     })
@@ -15,21 +18,21 @@ function animatedForm() {
 
 function validateUser(user){
     if(user.value.length < 6){
-        error('rgb(189, 87, 87');
+        error('rgba(240, 52, 52, 1');
     } else {
         error('rgba(87,189,130');
         return true;
     }
 }
 
-function nextSlide(parent, nextForm) {
+function nextForm(parent, nextForm) {
     parent.classList.add('innactive')
     parent.classList.remove('active')
     nextForm.classList.add('active')
 }
 
 function error(color) {
-    document.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
 }
 
 animatedForm();
